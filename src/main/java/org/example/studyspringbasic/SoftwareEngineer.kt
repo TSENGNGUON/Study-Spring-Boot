a1cb078a2c86538eede911cde8ab446b4f3c901e
nguonn <sengn5112@gmail.com>
2025-11-05 02:33:40 +0700

    started a little bit

---
+++ src/main/java/org/example/studyspringbasic/SoftwareEngineer.kt
@@ -0,0 +1,5 @@
+package org.example.studyspringbasic
+
+public class SoftwareEngineer {
+    private Integer id;
+}
---
+++ src/main/java/org/example/studyspringbasic/StudySpringBasicApplication.java
@@ -0,0 +1,21 @@
+package org.example.studyspringbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class StudySpringBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudySpringBasicApplication.class, args);
+    }
+
+    @GetMapping
+    public String hellWord(){
+        return "Hello World";
+    }
+
+}
